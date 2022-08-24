cf0ba96af1c7c2ba66b7b8dd0c4141478b19b0f2
Sakthivelan <sakthivelan89@gmail.com>
2022-08-24 14:24:02 +0530

    Update

--- src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -1,8 +1,6 @@
 package com.acme.mytrader.strategy;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -12,9 +10,7 @@ import org.mockito.Mock;
 import com.acme.mytrader.execution.ExecutionService;
 
 public class TradingStrategyTest {
-    @Test
-    public void testNothing() {
-    }
+    
     @Mock
     private TradingStrategy tStrategy;
     
@@ -25,14 +21,14 @@ public class TradingStrategyTest {
     @Test
     public void testTCSBuyScenario() throws Throwable {
     	
-        doNothing().when(tStrategy).afterReturningAdvice("TCS", 50.0);
+        doNothing().when(tStrategy).priceUpdate("TCS", 50);
         verify(eService, times(1)).buy("TCS",50.0,100);
 
     }
 
     @Test
     public void testTCSSellScenario() throws Throwable {
-    	 doNothing().when(tStrategy).afterReturningAdvice("TCS", 60.0);
+    	 doNothing().when(tStrategy).priceUpdate("TCS", 60.0);
          verify(eService, times(1)).sell("TCS",60.0,100);
     }
 }
